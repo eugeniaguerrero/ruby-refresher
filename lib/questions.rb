3209969b5c4a375f159b86585fcb2d27cc037b43
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-05-01 22:41:26 +0100

    adds first set of exercises

--- lib/questions.rb
+++ lib/questions.rb
@@ -1,38 +1,46 @@
 # keep only the elements that start with an a
 def select_elements_starting_with_a(array)
+  letter = 'a'
+  array.select { |element| element[0] == letter }
 end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
+  # array.select { |element| element[0] == %w(a e i o u) }
 end
 
 # remove instances of nil (but NOT false) from an array
 def remove_nils_from_array(array)
+  array.compact
 end
 
 # remove instances of nil AND false from an array
 def remove_nils_and_false_from_array(array)
+  array.select { |element| element != nil && element != false }
 end
 
 # don't reverse the array, but reverse every word inside it. e.g.
 # ['dog', 'monkey'] becomes ['god', 'yeknom']
 def reverse_every_element_in_array(array)
+  array.map {|element| element.reverse}
 end
 
 # given an array of student names, like ['Bob', 'Dave', 'Clive']
 # give every possible pairing - in this case:
 # [['Bob', 'Clive'], ['Bob', 'Dave'], ['Clive', 'Dave']]
-# make sure you don't have the same pairing twice, 
+# make sure you don't have the same pairing twice,
 def every_possible_pairing_of_students(array)
 end
 
-# discard the first 3 elements of an array, 
+# discard the first 3 elements of an array,
 # e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
 def all_elements_except_first_3(array)
+  array.slice(3..6)
 end
 
 # add an element to the beginning of an array
 def add_element_to_beginning_of_array(array, element)
+  array.unshift(element)
 end
 
 # sort an array of words by their last letter, e.g.
@@ -87,8 +95,6 @@ end
 def turn_symbol_into_string(symbol)
 end
 
-# get the average from an array, rounded to the nearest integer
-# so [10, 15, 25] should return 17
 def average_of_array(array)
 end
 
